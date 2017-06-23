f302e3fd4842dd3d78b49eaed211256bad3c3a34
ravigadila <ravi@micropyramid.com>
2017-06-23 14:34:00 +0530

    sample working code for socket.io

--- python/caps/it_works.py
+++ python/caps/it_works.py
@@ -1,5 +1,13 @@
 from wsgiref.simple_server import make_server, demo_app
 
+
+def simple_app(environ, start_response):
+    """Simplest possible application object"""
+    status = '200 OK'
+    response_headers = [('Content-type', 'text/plain')]
+    start_response(status, response_headers)
+    return [b"Hello World !"]
+
 if __name__ == '__main__':
-    server = make_server('', 5000, demo_app)
+    server = make_server('', 5000, simple_app)
     server.serve_forever()
